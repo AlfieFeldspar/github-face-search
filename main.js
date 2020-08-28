7341bf7c7c2beba27c9a0fac64794d6c81cbe8c5
Aissa Feldmann <aissaf@umich.edu>
2020-08-27 21:10:38 -0400

    face search connected to fb react github

--- main.js
+++ main.js
@@ -1,69 +1,58 @@
-// let books = [
-//   {
-//     title: 'Harry Potter',
-//     author: 'J.K. Rowling',
-//     imageURL:
-//       'https://books.google.com/books/content?id=WV8pZj_oNBwC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
-//     isbn: '9781921479311',
-//     pageCount: 268,
-//   },
-// ];
+let contributors = [
+  // {
+  //   img: 'https://avatars1.githubusercontent.com/u/1404810?v=4',
+  //   login: 'SimenB',
+  // },
+];
 
 // const Handlebars = require('handlebars'); //do not need if handlebars cdn is in html
 
-let renderFaces = function () {
-  console.log('rendering!');
+const addContributorToArray = (data) => {
+  // debugger;
 
-  $('.faces').empty();
-  let source = $('#face-template').html();
+  const newContributor = {
+    img: data.author.avatar_url,
+    login: data.author.login,
+  };
 
-  console.log(source);
+  contributors.push(newContributor);
+};
 
-  const template = Handlebars.compile(source);
+const renderContributors = () => {
+  $('#commits').empty();
 
-  for (let i = 0; i < renderFaces.length; i++) {
-    // create HTML and append to .faces
-    let newHTML = template(faces[i]);
-    $('.faces').append(newHTML);
+  for (let i = 0; i < contributors.length; i++) {
+    let contributor = contributors[i];
+
+    const source = $('#contributor-template').html();
+    const template = Handlebars.compile(source);
+    const contributorHTML = template(contributor);
+
+    $('#commits').append(contributorHTML);
   }
 };
 
-var addFaces = function (data) {
-  faces = [];
-
-  for (var i = 0; i < data.items.length; i++) {
-    var faceData = data.items[i];
-    
-    var face = {
-      login: faceData.author.login || null,
-      avatar_url: faceData.author.avatar_url || null,
-    };
-
-    faces.push(face);
-  }
-  renderFaces();
-};
-
-$('.search').on('click', function () {
-  var userSearch = $('#search-query').val();
-  // console.log("usersearch val: " + userSearch);
-  fetch(userSearch);
-});
-
-var fetch = function (query) {
+const fetchData = (sha) => {
   $.ajax({
-    method: "GET",
-    url: "https://api.github.com/repos/facebook/react/commits/" + query,
-    dataType: "json",
-    success: function(data) {
-      addFaces(data);
+    method: 'GET',
+    url: 'https://api.github.com/repos/facebook/react/commits/' + sha,
+    dataType: 'json',
+    success: function (data) {
+      // console.log(data);
+      addContributorToArray(data);
+      renderContributors();
     },
-    error: function(jqXHR, textStatus, errorThrown) {
+    error: function (jqXHR, textStatus, errorThrown) {
       console.log(textStatus);
-    }
+    },
   });
 };
 
+$('.search').on('click', function () {
+  console.log('clicked');
+  const sha = $('#sha').val();
+  // console.log("usersearch val: " + userSearch);
+  fetchData(sha);
+});
 
-
-
+// renderContributors();
